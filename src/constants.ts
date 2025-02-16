b3f84a9e2e64f95ec6257b2af903b9a96d4a53bc
gamejoye <gamejoye@gmail.com>
2025-02-16 21:39:50 +0800

    feat(constant): IDB_MODE constant

--- src/constants.ts
+++ src/constants.ts
@@ -4,3 +4,9 @@ export const DEFAULT_CONFIG: Required<ConfigOptions> = {
   storeName: 'storeage',
   version: 1,
 };
+
+export const IDB_MODE = {
+  READ_ONLY: 'readonly',
+  READ_WRITE: 'readwrite',
+  VERSION_CHANGE: 'versionchange',
+} as const;
