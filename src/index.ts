574017e021c5984db7b0c466c926c89eb24bb91e
gamejoye <gamejoye@gmail.com>
2025-02-17 14:43:53 +0800

    feat: integrate apis and unify export

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,67 @@
+import { idbDriver } from './drivers/idb';
+import { localstorageDriver } from './drivers/localstorage';
 
+const driversMap: [Drivers, IDriver][] = [
+  ['idb', idbDriver],
+  ['localstorage', localstorageDriver],
+];
+
+const defaultDriversSequence: Drivers[] = ['idb', 'localstorage'];
+let configDriversSequence: Drivers[] = [];
+
+const getDriver = () => {
+  for (const driver of configDriversSequence) {
+    const iDriver = driversMap.find(item => item[0] === driver);
+    if (iDriver /** TODO 并且支持该驱动程序 */) {
+      return iDriver[1];
+    }
+  }
+
+  for (const driver of defaultDriversSequence) {
+    const iDriver = driversMap.find(item => item[0] === driver);
+    if (iDriver /** TODO 并且支持该驱动程序 */) {
+      return iDriver[1];
+    }
+  }
+
+  // 如果没有任何驱动程序支持，则使用 localstorage 驱动程序
+  return localstorageDriver;
+};
+
+const config: IDriver['config'] = (options: ConfigOptions = {}) => {
+  if (options.driver) {
+    configDriversSequence = options.driver;
+  }
+  options = {
+    ...options,
+    driver: configDriversSequence.length ? configDriversSequence : defaultDriversSequence,
+  };
+  return getDriver().config(options);
+};
+
+const getItem: IDriver['getItem'] = (...args) => {
+  return getDriver().getItem(...args);
+};
+const setItem: IDriver['setItem'] = (...args) => {
+  const idriver = getDriver();
+  return idriver.setItem(...args);
+};
+const removeItem: IDriver['removeItem'] = (...args) => {
+  return getDriver().removeItem(...args);
+};
+const clear: IDriver['clear'] = (...args) => {
+  return getDriver().clear(...args);
+};
+
+const ready: IDriver['clear'] = (...args) => {
+  return getDriver().ready(...args);
+};
+
+export default {
+  config,
+  getItem,
+  setItem,
+  removeItem,
+  clear,
+  ready,
+};
