1b5f65205f4f25208fa3bc1687f5d7ed1e54ba89
gamejoye <gamejoye@gmail.com>
2025-02-17 15:18:09 +0800

    refactor(apis): oop encapsulation

--- src/index.ts
+++ src/index.ts
@@ -2,67 +2,66 @@ import { idbDriver } from './drivers/idb';
 import { localstorageDriver } from './drivers/localstorage';
 import { INTERNAL_DRIVERS } from './constants';
 
-const driversMap: [string, IDriver][] = [
-  [INTERNAL_DRIVERS.IDB, idbDriver],
-  [INTERNAL_DRIVERS.LOCALSTORAGE, localstorageDriver],
-];
+class Storeage {
+  private driversMap: [string, IDriver][] = [
+    [INTERNAL_DRIVERS.IDB, idbDriver],
+    [INTERNAL_DRIVERS.LOCALSTORAGE, localstorageDriver],
+  ];
+  private defaultDriversSequence = [INTERNAL_DRIVERS.IDB, INTERNAL_DRIVERS.LOCALSTORAGE];
+  private configDriversSequence: string[] = [];
 
-const defaultDriversSequence = [INTERNAL_DRIVERS.IDB, INTERNAL_DRIVERS.LOCALSTORAGE];
-let configDriversSequence: string[] = [];
-
-const getDriver = () => {
-  for (const driver of configDriversSequence) {
-    const iDriver = driversMap.find(item => item[0] === driver);
-    if (iDriver /** TODO 并且支持该驱动程序 */) {
-      return iDriver[1];
+  getDriver() {
+    for (const driver of this.configDriversSequence) {
+      const iDriver = this.driversMap.find(item => item[0] === driver);
+      if (iDriver /** TODO 并且支持该驱动程序 */) {
+        return iDriver[1];
+      }
     }
-  }
 
-  for (const driver of defaultDriversSequence) {
-    const iDriver = driversMap.find(item => item[0] === driver);
-    if (iDriver /** TODO 并且支持该驱动程序 */) {
-      return iDriver[1];
+    for (const driver of this.defaultDriversSequence) {
+      const iDriver = this.driversMap.find(item => item[0] === driver);
+      if (iDriver /** TODO 并且支持该驱动程序 */) {
+        return iDriver[1];
+      }
     }
+
+    // 如果没有任何驱动程序支持，则使用 localstorage 驱动程序
+    return localstorageDriver;
   }
 
-  // 如果没有任何驱动程序支持，则使用 localstorage 驱动程序
-  return localstorageDriver;
-};
-
-const config: IDriver['config'] = (options: ConfigOptions = {}) => {
-  if (options.driver) {
-    configDriversSequence = options.driver;
-  }
-  options = {
-    ...options,
-    driver: configDriversSequence.length ? configDriversSequence : defaultDriversSequence,
+  config: IDriver['config'] = (options: ConfigOptions = {}) => {
+    if (options.driver) {
+      this.configDriversSequence = options.driver;
+    }
+    options = {
+      ...options,
+      driver: this.configDriversSequence.length
+        ? this.configDriversSequence
+        : this.defaultDriversSequence,
+    };
+    return this.getDriver().config(options);
   };
-  return getDriver().config(options);
-};
 
-const getItem: IDriver['getItem'] = (...args) => {
-  return getDriver().getItem(...args);
-};
-const setItem: IDriver['setItem'] = (...args) => {
-  const idriver = getDriver();
-  return idriver.setItem(...args);
-};
-const removeItem: IDriver['removeItem'] = (...args) => {
-  return getDriver().removeItem(...args);
-};
-const clear: IDriver['clear'] = (...args) => {
-  return getDriver().clear(...args);
-};
+  getItem: IDriver['getItem'] = (...args) => {
+    return this.getDriver().getItem(...args);
+  };
+  setItem: IDriver['setItem'] = (...args) => {
+    return this.getDriver().setItem(...args);
+  };
 
-const ready: IDriver['clear'] = (...args) => {
-  return getDriver().ready(...args);
-};
+  removeItem: IDriver['removeItem'] = (...args) => {
+    return this.getDriver().removeItem(...args);
+  };
 
-export default {
-  config,
-  getItem,
-  setItem,
-  removeItem,
-  clear,
-  ready,
-};
+  clear: IDriver['clear'] = (...args) => {
+    return this.getDriver().clear(...args);
+  };
+
+  ready: IDriver['ready'] = (...args) => {
+    return this.getDriver().ready(...args);
+  };
+}
+
+const storeage = new Storeage();
+
+export default storeage;
